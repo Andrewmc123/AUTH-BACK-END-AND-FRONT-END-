83d76fbb755a5be09da6ad396f316069b8b78c31
Andrewmc123 <Letsmakeachange2015@gmail.com>
2025-03-16 13:20:56 -0400

    status report

--- Backend/db/seeders/20250314154627-demo-user.js
+++ Backend/db/seeders/20250314154627-demo-user.js
@@ -8,4 +8,32 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    options.tableName
+    options.tableName = 'Users'; 
+    return queryInterface.bulkInsert(options, [
+
+      {
+        email: 'demo@user.io', 
+        username: 'Demo-lition', 
+        hashedPassword: bcrypt.hashSync('password')
+      }, 
+      {
+        email: 'user1@user.io', 
+        username: 'FakeUser1', 
+        hasheedPassword: bcrypt.hashSync('password2')
+      }, 
+      {
+        email: 'user2user.io', 
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  }, 
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users'; 
+    const Op = Sequelize.Op; 
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+    }
+  };
